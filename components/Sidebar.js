570ae37ed00124ac29c5f88b67b3a126626c0ad7
r0ushan <roushan007kr@gmail.com>
2022-07-08 10:01:22 +0530

    sht

---
+++ components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ConnectButton } from 'web3uikit'
+
+const Sidebar = () => {
+    return (
+        <div>
+            <ConnectButton />
+
+        </div>
+    )
+}
+
+export default Sidebar
